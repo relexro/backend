083b1bfc87bb6a0afe01cc66aa3f5ef2798a68cc
Harmoniq Punk <spiny_05giggly@icloud.com>
2025-03-29 09:36:00 +0100

    wip

--- terraform/modules/firebase/main.tf
+++ terraform/modules/firebase/main.tf
@@ -9,4 +9,13 @@ resource "google_firebase_web_app" "default" {
   project      = var.project_id
   display_name = "Relex Web App"
   depends_on   = [google_firebase_project.default]
+}
+
+# Firestore Security Rules Deployment
+resource "google_firestore_security_rule" "rules" {
+  provider    = google-beta
+  project     = var.project_id
+  database    = "(default)"
+  rules_file  = "${path.module}/rules/firestore.rules"
+  depends_on  = [google_firebase_project.default]
 } 
--- terraform/modules/firebase/outputs.tf
+++ terraform/modules/firebase/outputs.tf
@@ -5,4 +5,9 @@ output "firebase_web_app_name" {
 output "firebase_web_app_id" {
   value = google_firebase_web_app.default.app_id
   sensitive = true
+}
+
+output "firestore_security_rules" {
+  value = google_firestore_security_rule.rules.id
+  description = "ID of the deployed Firestore security rules"
 } 
